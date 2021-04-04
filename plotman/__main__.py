07dc4be92c0cd2955c76241fe34b5569f309441d
Andrew Kubera <andrew.kubera@floss.email>
2021-04-04 17:01:48 -0400

    Use Configuration objects in main functions

--- plotman/__main__.py
+++ plotman/__main__.py
@@ -1,7 +1,7 @@
 """
 Main executable script
 """
-from typing import Union
+from typing import Union, List
 
 from datetime import datetime
 from io import TextIOWrapper
@@ -88,6 +88,8 @@ class Configuration:
     def __init__(self, data):
         self.data = data
 
+        self.polling_time = float(self.scheduling['polling_time_s'])
+
     @property
     def directories(self):
         return self.data['directories']
@@ -99,6 +101,10 @@ class Configuration:
     @property
     def plotting(self):
         return self.data['plotting']
+
+    @property
+    def log(self):
+        return self.data['directories']['log']
 
     @classmethod
     def from_file(cls, fp: Union[str, TextIOWrapper]):
@@ -130,92 +136,82 @@ def main(argv=None):
         parser.print_help(file=sys.stderr)
         return 1
 
-    return main_func(args, cfg.data)
+    return main_func(args, cfg)
     
 
-def main_plot(args, cfg):
+def main_plot(args, cfg: Configuration):
     """Stay alive, spawning plot jobs"""
-    dir_cfg = cfg['directories']
-    sched_cfg = cfg['scheduling']
-    plotting_cfg = cfg['plotting']
     print('...starting plot loop')
     while True:
-        wait_reason = manager.maybe_start_new_plot(dir_cfg, sched_cfg, plotting_cfg)
+        wait_reason = manager.maybe_start_new_plot(cfg.directories, cfg.scheduling, cfg.plotting)
 
         # TODO: report this via a channel that can be polled on demand, so we don't spam the console
-        sleep_s = int(sched_cfg['polling_time_s'])
+        sleep_s = cfg.polling_time
         if wait_reason:
-            print('...sleeping %d s: %s' % (sleep_s, wait_reason))
+            print('...sleeping %g s: %s' % (sleep_s, wait_reason))
 
         time.sleep(sleep_s)
 
 
-def main_status(args, cfg):
-    dir_cfg = cfg['directories']
-    jobs = Job.get_running_jobs(dir_cfg['log'])
+def main_status(args, cfg: Configuration):
+    jobs = Job.get_running_jobs(cfg.log)
     (rows, columns) = os.popen('stty size', 'r').read().split()
     print(reporting.status_report(jobs, int(columns)))
     return 0
 
 
-def main_analyze(args, cfg):
+def main_analyze(args, cfg: Configuration):
     """Analysis of completed jobs"""
     analyzer = LogAnalyzer()
     analyzer.analyze(args.logfile)
     return 0
 
 
-def main_dirs(args, cfg):
-    dir_cfg = cfg['directories']
-    sched_cfg = cfg['scheduling']
-    plotting_cfg = cfg['plotting']
-    jobs = Job.get_running_jobs(dir_cfg['log'])
+def main_dirs(args, cfg: Configuration):
+    jobs = Job.get_running_jobs(cfg.log)
     (rows, columns) = os.popen('stty size', 'r').read().split()
-    print(reporting.dirs_report(jobs, dir_cfg, sched_cfg, int(columns)))
+    print(reporting.dirs_report(jobs, cfg.directories, cfg.scheduling, int(columns)))
     return 0
 
 
-def main_interactive(args, cfg):
+def main_interactive(args, cfg: Configuration):
     interactive.run_interactive()
     return 0
 
 
-def main_dsched(args, cfg):
+def main_dsched(args, cfg: Configuration):
     """show the destination drive usage schedule"""
-    dir_cfg = cfg['directories']
-    jobs = Job.get_running_jobs(dir_cfg['log'])
+    dir_cfg = cfg.directories
+    jobs = Job.get_running_jobs(dir_cfg.log)
     for (d, ph) in manager.dstdirs_to_furthest_phase(jobs).items():
         print('  %s : %s' % (d, str(ph)))
     return 0
 
 
-def main_archive(args, cfg):
+def main_archive(args, cfg: Configuration):
     """Start running archival"""
-    dir_cfg = cfg['directories']
-    jobs = Job.get_running_jobs(dir_cfg['log'])
+    jobs = Job.get_running_jobs(cfg.log)
     print('...starting archive loop')
     firstit = True
     while True:
         if not firstit:
             print('Sleeping 60s until next iteration...')
             time.sleep(60)
-            jobs = Job.get_running_jobs(dir_cfg['log'])
+            jobs = Job.get_running_jobs(cfg.log)
         firstit = False
-        archive.archive(dir_cfg, jobs)
+        archive.archive(cfg.directories, jobs)
     return 0
 
 
-def main_details(args, cfg):
-    dir_cfg = cfg['directories']
-    selected = select_jobs(args, dir_cfg)
+def main_details(args, cfg: Configuration):
+    selected = select_jobs(args, cfg.directories)
     for job in selected:
         print(job.status_str_long())
     return 0
 
 
-def main_files(args, cfg):
-    dir_cfg = cfg['directories']
-    selected = select_jobs(args, dir_cfg)
+def main_files(args, cfg: Configuration):
+    selected = select_jobs(args, cfg.directories)
     for job in selected:
         temp_files = job.get_temp_files()
         for f in temp_files:
@@ -223,9 +219,8 @@ def main_files(args, cfg):
     return 0
 
 
-def main_kill(args, cfg):
-    dir_cfg = cfg['directories']
-    selected = select_jobs(args, dir_cfg)
+def main_kill(args, cfg: Configuration):
+    selected = select_jobs(args, cfg.directories)
     for job in selected:
         # First suspend so job doesn't create new files
         print('Pausing PID %d, plot id %s' % (job.proc.pid, job.plot_id))
@@ -245,25 +240,24 @@ def main_kill(args, cfg):
                 os.remove(f)
     return 0
 
-def main_suspend(args, cfg):
-    dir_cfg = cfg['directories']
-    selected = select_jobs(args, dir_cfg)
+
+def main_suspend(args, cfg: Configuration):
+    selected = select_jobs(args, cfg.directories)
     for job in selected:
         print('Suspending ' + job.plot_id)
         job.suspend()
     return 0
 
 
-def main_resume(args, cfg):
-    dir_cfg = cfg['directories']
-    selected = select_jobs(args, dir_cfg)
+def main_resume(args, cfg: Configuration):
+    selected = select_jobs(args, cfg.directories)
     for job in selected:
         print('Resuming ' + job.plot_id)
         job.resume()
     return 0
 
 
-def select_jobs(args, dir_cfg):
+def select_jobs(args, dir_cfg) -> List[Job]:
     """Select jobs based on partial id""" 
     jobs = Job.get_running_jobs(dir_cfg['log'])
 
